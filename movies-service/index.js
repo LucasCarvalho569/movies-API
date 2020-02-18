0e824a0971738dcebd9bfcddaa8f0a9ed9f23cb4
Lucas Carvalho <lucas.carvalho569@gmail.com>
2020-02-18 08:53:27 -0300

    prettier and eslint config for circleci

--- movies-service/index.js
+++ movies-service/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const enviroment = require("./enviroment");
+const enviroment = require('./enviroment');
 const app = express();
 
 app.listen(enviroment.port, () => {
