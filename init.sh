5e213d110df2a8a2554869a76a1160eefcf02e92
Lucas Carvalho <lucas.carvalho569@gmail.com>
2020-03-12 03:04:56 -0300

    Docker files - microservice communication

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 docker stop $(docker ps -a -q) && 
-docker-compose -f ./users-service/docker-compose.yml up -d && 
-docker-compose -f ./movies-service/docker-compose.yml up -d && 
-docker-compose -f ./logs-service/docker-compose.yml up
+docker-compose up --build -d &&
+docker-compose -f ./users-service/docker-compose.yml up --build -d && 
+docker-compose -f ./movies-service/docker-compose.yml up --build -d && 
+docker-compose -f ./logs-service/docker-compose.yml up --build
--- proxy.js
+++ proxy.js
@@ -6,6 +6,7 @@ const usersService = httpProxy(process.env.USERS_SERVICE);
 const moviesService = httpProxy(process.env.MOVIES_SERVICE);
 
 app.get('/users/*', (req, res, next) => usersService(req, res, next));
+
 app.get('/movies/*', (req, res, next) => moviesService(req, res, next));
 
 app.listen(process.env.PORT);
