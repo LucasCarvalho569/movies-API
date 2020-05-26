80156df79a8c44d22f161ec62621b5252654e4d3
Lucas Carvalho <lucas.carvalho569@gmail.com>
2020-05-26 11:01:32 -0300

    Database configuration files
    Add sequelize configuration files and model auto import for user-service

---
+++ users-service/config/database.js
@@ -0,0 +1,29 @@
+const enviroment = require('./enviroment');
+
+module.exports = {
+  development: {
+    username: enviroment.dbUser,
+    password: enviroment.dbPassword,
+    database: enviroment.dbName,
+    host: enviroment.dbHost,
+    dialect: 'mysql',
+    operatorsAliases: 0,
+    seederStorage: 'sequelize'
+  },
+  test: {
+    username: enviroment.dbUser,
+    password: enviroment.dbPassword,
+    database: enviroment.dbName,
+    host: enviroment.dbHost,
+    dialect: 'mysql',
+    operatorsAliases: 0
+  },
+  production: {
+    username: enviroment.dbUser,
+    password: enviroment.dbPassword,
+    database: enviroment.dbName,
+    host: enviroment.dbHost,
+    dialect: 'mysql',
+    operatorsAliases: 0
+  }
+};
--- users-service/config/enviroment.js
+++ users-service/config/enviroment.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT,
   enviroment: process.env.NODE_ENV,
-  dbHost: process.env.MYSQL_HOST,
+  dbHost: process.env.MYSQL_URL,
   dbPort: process.env.MYSQL_PORT,
-  dbUser: process.env.MYSQL_USER,
-  dbPassword: process.env.MYSQL_PASS
+  dbUser: process.env.MYSQL_USERNAME,
+  dbName: process.env.MYSQL_DB,
+  dbPassword: process.env.MYSQL_PASSWORD
 };
