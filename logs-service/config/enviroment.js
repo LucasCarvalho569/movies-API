cff196c91dc376f62de2e4edddd8b2e8efe03d04
Lucas Carvalho <lucas.carvalho569@gmail.com>
2020-06-07 22:33:52 -0300

    Mongo connection in logs service

--- logs-service/config/enviroment.js
+++ logs-service/config/enviroment.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT,
   enviroment: process.env.NODE_ENV,
-  dbHost: process.env.MONGO_HOST,
+  dbHost: process.env.MONGO_URL,
+  dbName: process.env.MONGO_DB,
   dbPort: process.env.MONGO_PORT,
-  dbUser: process.env.MONGO_USER,
-  dbPassword: process.env.MONGO_PASS
+  dbUser: process.env.MONGO_USERNAME,
+  dbPassword: process.env.MONGO_PASSWORD
 };
