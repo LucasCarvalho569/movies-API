b3444edfc75237d973281e96b437d794f4053dc3
Lucas Carvalho <lucas.carvalho569@gmail.com>
2020-08-07 08:10:15 -0300

    Docker command fixes

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-docker stop $(docker ps -a -q) && 
+docker container ls -aq | xargs --no-run-if-empty docker stop $(docker ps -a -q) && 
 docker-compose up --build -d &&
 docker-compose -f ./users-service/docker-compose.yml up --build -d && 
 docker-compose -f ./movies-service/docker-compose.yml up --build -d && 
